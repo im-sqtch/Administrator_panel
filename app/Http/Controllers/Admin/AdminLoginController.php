eee61a0d48b5b34b61b402c9cbded345e7c2f3dd
Gabriel Bomtempo <gabrielbomlima@gmail.com>
2022-09-19 16:50:23 -0300

    More authentication setup

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -20,4 +20,29 @@ class AdminLoginController extends Controller
     {
         return view('admin.forget_password');
     }
+
+    public function admin_login_submit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if(Auth::guard('admin')->attempt($credentials)) {
+            return redirect()->route('admin_home');
+        } else {
+            return redirect()->route('admin_login')->with('error', 'Information is not correct!');
+        }
+    }
+
+    public function admin_logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin_login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminHomeController;
 use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Middleware\Admin;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // Admin Routes
-Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
+Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home')->middleware('admin:admin');
 
 Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
+Route::post('/admin/login_submit', [AdminLoginController::class, 'admin_login_submit'])->name('admin_login_submit');
 Route::get('/admin/forget-password', [AdminLoginController::class, 'admin_forget_password'])->name('admin_forget_password');
+
+Route::get('/admin/logout', [AdminLoginController::class, 'admin_logout'])->name('admin_logout');
